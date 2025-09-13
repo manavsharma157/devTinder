13443d5753871352d7353b1234d96900ebb1fd21
Manav Sharma <manav157sh@gmail.com>
2025-09-13 20:09:17 +0530

    exploring vlaidator library

--- src/app.js
+++ src/app.js
@@ -1,19 +1,34 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express(); //instance of an express application
+// const app = express(); //instance of an express application
 
-//Server will respond to the incoming request from the browser on localhost:3000
-// By default it will even respond to localhost:3000/hello, localhost:3000/home....
-// app.use((req, res) => {
+// //Server will respond to the incoming request from the browser on localhost:3000
+// // By default it will even respond to localhost:3000/hello, localhost:3000/home....
+// // app.use((req, res) => {
+// //     res.send("Hello from the server");
+// // })
+
+// //specific to localhost:300/test
+// app.use("/test",(req, res) => {
 //     res.send("Hello from the server");
 // })
 
-//specific to localhost:300/test
-app.use("/test",(req, res) => {
-    res.send("Hello from the server");
-})
+const express = require("express");
 
-app.listen(3000, ()=> {
-    console.log("Server is successfully listening on port 3000...")
-});
+const app = express(); //instance of an express application
 
+//importing the database connection function
+const connectDB = require("./config/database");
+
+/////////////////////////////////////////////////
+//FIrst connect to the database and then start the server
+connectDB()
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(3000, () => {
+      console.log("Server is successfully listening on port 3000...");
+    });
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
---
+++ src/config/database.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://manav157sh_db_user:Manav1234@test.zuwnpwj.mongodb.net/"
+);
+//Connects to the db cluster
+// This function returns a promise
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://manav157sh_db_user:Manav1234@test.zuwnpwj.mongodb.net/"
+  );  
+};
+
+module.exports = connectDB;
+
+// connectDB()
+// .then(() => {
+//     console.log("MongoDB connected");
+// })
+// .catch((err) => {
+//     console.log("Failed to connect to MongoDB", err);
+// });
+
+// module.exports = connectDB;
