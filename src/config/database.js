c8a93d3485133087cd35ac3f5559047e404f54fb
Manav Sharma <manav157sh@gmail.com>
2026-03-12 18:39:24 +0530

    Updates in Mongo DB Connection string

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect(
-//   "mongodb+srv://manav157sh_db_user:Manav1234@test.zuwnpwj.mongodb.net/devTinder"
-// );
 //Connects to the db cluster
 // This function returns a promise
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://manav157sh_db_user:Manav1234@test.zuwnpwj.mongodb.net/devTinder"
-  );  
+  try {
+    await mongoose.connect(
+      "mongodb://manav157sh_db_user:Manav1234@ac-cynnoen-shard-00-00.zuwnpwj.mongodb.net:27017,ac-cynnoen-shard-00-01.zuwnpwj.mongodb.net:27017,ac-cynnoen-shard-00-02.zuwnpwj.mongodb.net:27017/devTinder?ssl=true&replicaSet=atlas-7lnpfs-shard-0&authSource=admin&appName=test"
+    );
+    console.log("Atlas Database Connected Successfully!");
+  } catch (err) {
+    console.error("Atlas Connection Error:", err.message);
+  }
 };
+
 
 module.exports = connectDB;
 
